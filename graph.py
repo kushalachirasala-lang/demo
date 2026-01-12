d57b89d04100eb5dd534c9698d03d9be517c0e15
Arjun3618 <arjunakula795@gmail.com>
2026-01-12 13:10:53 +0530

    Added graph file

---
+++ graph.py
@@ -0,0 +1,22 @@
+import sqlite3
+import matplotlib.pyplot as plt
+
+# Connect to the SQLite database
+conn = sqlite3.connect("reviews.db")
+cur = conn.cursor()
+
+# Fetch ratings from the reviews table
+cur.execute("SELECT * FROM reviews")
+ratings = [row[0] for row in cur.fetchall()]
+conn.close()
+
+# Plot histogram of ratings
+plt.figure(figsize=(6,4))
+plt.hist(ratings, bins=range(1,7), edgecolor="black", align="left")
+plt.title("Rating Distribution")
+plt.xlabel("Rating (1-5)")
+plt.ylabel("Number of Reviews")
+plt.xticks(range(1,6))
+
+# Show the plot
+plt.show()
